020559d21dda75b2c9520e0d9e9aeb08e4a09fc2
Brian Schiller <bgschiller@gmail.com>
2025-02-11 23:01:49 -0800

    add redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,12 @@ import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
+  redirects: {
+    "/blog/2024/12/02/constraint-solving-in-spreadsheets": {
+      status: 301,
+      destination: "/blog/2025/02/11/constraint-solving-in-spreadsheets",
+    },
+  },
   site: "https://brianschiller.com",
   integrations: [
     mdx(),
